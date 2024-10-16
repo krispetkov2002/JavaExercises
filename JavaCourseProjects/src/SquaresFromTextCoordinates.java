9b999dacbc6deeb7e8cdafa0242c50e8d173c20e
Kristiyan Petkov <47471994+krispetkov2002@users.noreply.github.com>
2024-10-16 16:00:51 +0300

    1.1
    Добавяне на функционалност за вземане на координатите на 6-те квадрата от текстов файл. Мащабриране и фиксване на размерите и позициите на точките на квадратите, понеже се използва fillPolygon(), вместо fillRect().

--- JavaCourseProjects/src/SquaresFromTextCoordinates.java
+++ JavaCourseProjects/src/SquaresFromTextCoordinates.java
@@ -30,12 +30,16 @@ public class SquaresFromTextCoordinates extends Frame {
 
         Color[] _colors = new Color[] { Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW,
                 Color.ORANGE, Color.PINK };
+
         for (int i = 0; i < _coordinates.size(); i++) {
             g.setColor(_colors[i]);
-            g.fillRect(_coordinates.get(i).Xo,
-                    _coordinates.get(i).Yo,
-                    _coordinates.get(i).Wo,
-                    _coordinates.get(i).Ho);
+            int[] x_coordinates = {_coordinates.get(i).X0, _coordinates.get(i).X1,
+                    _coordinates.get(i).X2, _coordinates.get(0).X3};
+
+            int[] y_coordinates = {_coordinates.get(i).Y0, _coordinates.get(i).Y1,
+                    _coordinates.get(i).Y2, _coordinates.get(0).Y3};
+
+            g.fillPolygon(x_coordinates, y_coordinates, 4);
         }
     }
 
@@ -43,29 +47,60 @@ public class SquaresFromTextCoordinates extends Frame {
         FileReader fileReader = new FileReader("src\\SquaresCoordinates.txt");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String textLine = bufferedReader.readLine();
+
+        boolean flag = true;
         while (textLine != null)
         {
             String[] textCoordinates = textLine.split(" ");
-            Coordinates coordinates = new Coordinates(Integer.parseInt(textCoordinates[0]),
-                    Integer.parseInt(textCoordinates[1]),
-                    Integer.parseInt(textCoordinates[2]),
-                    Integer.parseInt(textCoordinates[3]));
-            _coordinates.add(coordinates);
+            if(flag){
+                Coordinates coordinates = new Coordinates(Integer.parseInt(textCoordinates[0]),
+                        Integer.parseInt(textCoordinates[1]),
+                        Integer.parseInt(textCoordinates[2]),
+                        Integer.parseInt(textCoordinates[3]),
+                        Integer.parseInt(textCoordinates[4]),
+                        Integer.parseInt(textCoordinates[5]),
+                        Integer.parseInt(textCoordinates[6]),
+                        Integer.parseInt(textCoordinates[7]));
+                _coordinates.add(coordinates);
+                flag = false;
+            }
+            else{
+                Coordinates coordinates = new Coordinates(Integer.parseInt(textCoordinates[0]),
+                        Integer.parseInt(textCoordinates[1]),
+                        Integer.parseInt(textCoordinates[2]),
+                        Integer.parseInt(textCoordinates[3]),
+                        Integer.parseInt(textCoordinates[4]),
+                        Integer.parseInt(textCoordinates[5]));
+                _coordinates.add(coordinates);
+            }
+
+
             textLine = bufferedReader.readLine();
         }
     }
 }
 
 class Coordinates {
-    public int Xo, Yo;
-    public int Wo;
-    public int Ho;
+    public int X0, Y0, X1, Y1, X2, Y2, X3, Y3;
+    //public int Wo;
+    //public int Ho;
 
-    public Coordinates(int X, int Y, int W, int H) {
-        this.Xo = X;
-        this.Yo = Y;
-
-        this.Wo = W;
-        this.Ho = H;
+    public Coordinates(int X0, int Y0, int X1, int Y1, int X2, int Y2, int X3, int Y3) {
+        this.X0 = X0;
+        this.Y0 = Y0;
+        this.X1 = X1;
+        this.Y1 = Y1;
+        this.X2 = X2;
+        this.Y2 = Y2;
+        this.X3 = X3;
+        this.Y3 = Y3;
+    }
+    public Coordinates(int X0, int Y0, int X1, int Y1, int X2, int Y2) {
+        this.X0 = X0;
+        this.Y0 = Y0;
+        this.X1 = X1;
+        this.Y1 = Y1;
+        this.X2 = X2;
+        this.Y2 = Y2;
     }
 }
